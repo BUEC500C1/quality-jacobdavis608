c9ae743f037a85bfe392ee2fc99611672075f36e
Jacob Davis <jacobdavis608@gmail.com>
2020-01-27 16:22:37 -0500

    Adding command line argument checking

--- num_convert.py
+++ num_convert.py
@@ -1,9 +1,28 @@
 # EC 500 Building Software
 # Jacob Davis Assignment 1
 # Spring 2020
+import sys
+
+def convert_to_rom(arabic_nums):
+    '''
+    Converts input Arabic numerals into Roman numerals. Returns a list 
+    of strings representing the Roman numerals.
+    '''
+    # Convert all arabic nums
+    for num in arabic_nums:
+        print(num)
 
 
-def convert():
-    # Check to see if an input is provided via command line
 
-    # Convert input
+    return arabic_nums
+    
+
+def main():
+    # Check the command line for arguments
+    if (len(sys.argv) <= 1):
+        print("No arguments provided\n")
+    else: #pass arguments to conversion
+        roman_numerals = convert_to_rom(sys.argv[1:])
+
+if __name__ == "__main__":
+    main()
